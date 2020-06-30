5d0f4a0abbcd313cb742d69d93712062ed3e198a
SynchronClover <fernanmc@rose-hulman.edu>
2020-06-29 21:05:46 -0400

    Progress on Unit 8. Module 1-3 completed, but some failed tests on Module 1, not sure why.

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,8 +2,8 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Marc Fernandez.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -39,7 +39,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# DONE: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
@@ -90,7 +90,27 @@ def main():
 #
 ########################################################################
 
+class Baby(object):
+    def __init__(self,name):
+        self.name=name
 
+        print('Hello baby, {}!'.format(name))
+        self.count=1
+
+    def feed_baby(self):
+        print('Thank you for feeding baby {}.'.format(self.name))
+        self.count=1
+
+    def hour_passes(self):
+        count=self.count
+
+        if count==1:
+            print('Baby {} is sleeping'.format(self.name))
+        elif count==2:
+            print('Baby {} is awake. Time for food'.format(self.name))
+        elif count>=3:
+            print('Baby {} is CRYING uncontrollably! Feed the baby!'.format(self.name))
+        self.count=self.count+1
 
 
 
